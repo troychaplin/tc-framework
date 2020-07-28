8a6260c06180535de337b42fdd20ad03b8fb6238
Troy Chaplin <troy.chaplin@gmail.com>
2020-07-27 22:38:50 -0400

    Added block testing file

--- index.php
+++ index.php
@@ -2,10 +2,11 @@
 require 'header.php';
 
 echo '<main>';
+require '_examples/grid.php';
+require '_examples/blocks.php';
 require '_examples/content.php';
 // require '_examples/images.php';
 require '_examples/wp-images.php';
-require '_examples/grid.php';
 echo '</main>';
 
 require 'footer.php';
