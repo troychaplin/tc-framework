c48943d69b174bf9d74f832152a4f5774fec15a9
Troy Chaplin <troy.chaplin@gmail.com>
2021-06-28 21:52:23 -0400

    Simplified block and grid classes

--- _examples/grid.php
+++ _examples/grid.php
@@ -1,6 +1,3 @@
-<h2>Grid with even number of items</h2>
-
-<h3>Grid outside of u-block</h3>
 <div class="u-grid u-grid--2">
     <div style="background-color: #d9d9d9; padding: 15px;">1</div>
     <div style="background-color: #d9d9d9; padding: 15px;">2</div>
@@ -8,39 +5,6 @@
     <div style="background-color: #d9d9d9; padding: 15px;">4</div>
 </div>
 
-<div class="u-block u-block--white">
-    <h3>Grid inside of u-block</h3>
-    <div class="u-grid u-grid--3">
-        <div style="background-color: #d9d9d9; padding: 15px;">1</div>
-        <div style="background-color: #d9d9d9; padding: 15px;">2</div>
-        <div style="background-color: #d9d9d9; padding: 15px;">3</div>
-        <div style="background-color: #d9d9d9; padding: 15px;">4</div>
-        <div style="background-color: #d9d9d9; padding: 15px;">5</div>
-        <div style="background-color: #d9d9d9; padding: 15px;">6</div>
-    </div>
-</div>
-
-<div class="u-block u-block--grey">
-    <h3>Grid inside of u-block--grey</h3>
-    <div class="u-grid u-grid--4">
-        <div style="background-color: #d9d9d9; padding: 15px;">1</div>
-        <div style="background-color: #d9d9d9; padding: 15px;">2</div>
-        <div style="background-color: #d9d9d9; padding: 15px;">3</div>
-        <div style="background-color: #d9d9d9; padding: 15px;">4</div>
-        <div style="background-color: #d9d9d9; padding: 15px;">5</div>
-        <div style="background-color: #d9d9d9; padding: 15px;">6</div>
-        <div style="background-color: #d9d9d9; padding: 15px;">7</div>
-        <div style="background-color: #d9d9d9; padding: 15px;">8</div>
-    </div>
-</div>
-
-<h3>Consecutive grids outside of u-block</h3>
-<div class="u-grid u-grid--2">
-    <div style="background-color: #d9d9d9; padding: 15px;">1</div>
-    <div style="background-color: #d9d9d9; padding: 15px;">2</div>
-    <div style="background-color: #d9d9d9; padding: 15px;">3</div>
-    <div style="background-color: #d9d9d9; padding: 15px;">4</div>
-</div>
 <div class="u-grid u-grid--3">
     <div style="background-color: #d9d9d9; padding: 15px;">1</div>
     <div style="background-color: #d9d9d9; padding: 15px;">2</div>
@@ -50,8 +14,7 @@
     <div style="background-color: #d9d9d9; padding: 15px;">6</div>
 </div>
 
-<div class="u-block u-block--white">
-    <h3>Consecutive grids inside of u-block</h3>
+<div class="u-block">
     <div class="u-grid u-grid--2">
         <div style="background-color: #d9d9d9; padding: 15px;">1</div>
         <div style="background-color: #d9d9d9; padding: 15px;">2</div>
@@ -66,59 +29,18 @@
         <div style="background-color: #d9d9d9; padding: 15px;">5</div>
         <div style="background-color: #d9d9d9; padding: 15px;">6</div>
     </div>
-    <div class="u-grid u-grid--4">
-        <div style="background-color: #d9d9d9; padding: 15px;">1</div>
-        <div style="background-color: #d9d9d9; padding: 15px;">2</div>
-        <div style="background-color: #d9d9d9; padding: 15px;">3</div>
-        <div style="background-color: #d9d9d9; padding: 15px;">4</div>
-        <div style="background-color: #d9d9d9; padding: 15px;">5</div>
-        <div style="background-color: #d9d9d9; padding: 15px;">6</div>
-        <div style="background-color: #d9d9d9; padding: 15px;">7</div>
-        <div style="background-color: #d9d9d9; padding: 15px;">8</div>
-    </div>
 </div>
 
-<div class="u-block u-block--grey">
-    <h3>Consecutive grids inside of u-block--grey</h3>
-    <div class="u-grid u-grid--2">
-        <div style="background-color: #d9d9d9; padding: 15px;">1</div>
-        <div style="background-color: #d9d9d9; padding: 15px;">2</div>
-        <div style="background-color: #d9d9d9; padding: 15px;">3</div>
-        <div style="background-color: #d9d9d9; padding: 15px;">4</div>
-    </div>
-    <div class="u-grid u-grid--3">
-        <div style="background-color: #d9d9d9; padding: 15px;">1</div>
-        <div style="background-color: #d9d9d9; padding: 15px;">2</div>
-        <div style="background-color: #d9d9d9; padding: 15px;">3</div>
-        <div style="background-color: #d9d9d9; padding: 15px;">4</div>
-        <div style="background-color: #d9d9d9; padding: 15px;">5</div>
-        <div style="background-color: #d9d9d9; padding: 15px;">6</div>
-    </div>
-    <div class="u-grid u-grid--4">
-        <div style="background-color: #d9d9d9; padding: 15px;">1</div>
-        <div style="background-color: #d9d9d9; padding: 15px;">2</div>
-        <div style="background-color: #d9d9d9; padding: 15px;">3</div>
-        <div style="background-color: #d9d9d9; padding: 15px;">4</div>
-        <div style="background-color: #d9d9d9; padding: 15px;">5</div>
-        <div style="background-color: #d9d9d9; padding: 15px;">6</div>
-        <div style="background-color: #d9d9d9; padding: 15px;">7</div>
-        <div style="background-color: #d9d9d9; padding: 15px;">8</div>
-    </div>
-</div>
-
-<h2>Grid with uneven number of items</h2>
-
-<h3>Grid outside of u-block</h3>
 <div class="u-grid u-grid--2">
     <div style="background-color: #d9d9d9; padding: 15px;">1</div>
     <div style="background-color: #d9d9d9; padding: 15px;">2</div>
     <div style="background-color: #d9d9d9; padding: 15px;">3</div>
     <div style="background-color: #d9d9d9; padding: 15px;">4</div>
-    <div style="background-color: #d9d9d9; padding: 15px;">5</div>
 </div>
 
-<div class="u-block u-block--white">
-    <h3>Grid inside of u-block</h3>
+<div class="u-block">
+    <p>fsafasgfas</p>
+    <p>fsafasgfas</p>
     <div class="u-grid u-grid--3">
         <div style="background-color: #d9d9d9; padding: 15px;">1</div>
         <div style="background-color: #d9d9d9; padding: 15px;">2</div>
@@ -126,101 +48,7 @@
         <div style="background-color: #d9d9d9; padding: 15px;">4</div>
         <div style="background-color: #d9d9d9; padding: 15px;">5</div>
         <div style="background-color: #d9d9d9; padding: 15px;">6</div>
-        <div style="background-color: #d9d9d9; padding: 15px;">7</div>
     </div>
-</div>
-
-<div class="u-block u-block--grey">
-    <h3>Grid inside of u-block--grey</h3>
-    <div class="u-grid u-grid--4">
-        <div style="background-color: #d9d9d9; padding: 15px;">1</div>
-        <div style="background-color: #d9d9d9; padding: 15px;">2</div>
-        <div style="background-color: #d9d9d9; padding: 15px;">3</div>
-        <div style="background-color: #d9d9d9; padding: 15px;">4</div>
-        <div style="background-color: #d9d9d9; padding: 15px;">5</div>
-        <div style="background-color: #d9d9d9; padding: 15px;">6</div>
-        <div style="background-color: #d9d9d9; padding: 15px;">7</div>
-        <div style="background-color: #d9d9d9; padding: 15px;">8</div>
-        <div style="background-color: #d9d9d9; padding: 15px;">9</div>
-    </div>
-</div>
-
-<h3>Consecutive grids outside of u-block</h3>
-<div class="u-grid u-grid--2">
-    <div style="background-color: #d9d9d9; padding: 15px;">1</div>
-    <div style="background-color: #d9d9d9; padding: 15px;">2</div>
-    <div style="background-color: #d9d9d9; padding: 15px;">3</div>
-    <div style="background-color: #d9d9d9; padding: 15px;">4</div>
-    <div style="background-color: #d9d9d9; padding: 15px;">5</div>
-</div>
-<div class="u-grid u-grid--3">
-    <div style="background-color: #d9d9d9; padding: 15px;">1</div>
-    <div style="background-color: #d9d9d9; padding: 15px;">2</div>
-    <div style="background-color: #d9d9d9; padding: 15px;">3</div>
-    <div style="background-color: #d9d9d9; padding: 15px;">4</div>
-    <div style="background-color: #d9d9d9; padding: 15px;">5</div>
-    <div style="background-color: #d9d9d9; padding: 15px;">6</div>
-    <div style="background-color: #d9d9d9; padding: 15px;">7</div>
-</div>
-
-<div class="u-block u-block--white">
-    <h3>Consecutive grids inside of u-block</h3>
-    <div class="u-grid u-grid--2">
-        <div style="background-color: #d9d9d9; padding: 15px;">1</div>
-        <div style="background-color: #d9d9d9; padding: 15px;">2</div>
-        <div style="background-color: #d9d9d9; padding: 15px;">3</div>
-        <div style="background-color: #d9d9d9; padding: 15px;">4</div>
-        <div style="background-color: #d9d9d9; padding: 15px;">5</div>
-    </div>
-    <div class="u-grid u-grid--3">
-        <div style="background-color: #d9d9d9; padding: 15px;">1</div>
-        <div style="background-color: #d9d9d9; padding: 15px;">2</div>
-        <div style="background-color: #d9d9d9; padding: 15px;">3</div>
-        <div style="background-color: #d9d9d9; padding: 15px;">4</div>
-        <div style="background-color: #d9d9d9; padding: 15px;">5</div>
-        <div style="background-color: #d9d9d9; padding: 15px;">6</div>
-        <div style="background-color: #d9d9d9; padding: 15px;">7</div>
-    </div>
-    <div class="u-grid u-grid--4">
-        <div style="background-color: #d9d9d9; padding: 15px;">1</div>
-        <div style="background-color: #d9d9d9; padding: 15px;">2</div>
-        <div style="background-color: #d9d9d9; padding: 15px;">3</div>
-        <div style="background-color: #d9d9d9; padding: 15px;">4</div>
-        <div style="background-color: #d9d9d9; padding: 15px;">5</div>
-        <div style="background-color: #d9d9d9; padding: 15px;">6</div>
-        <div style="background-color: #d9d9d9; padding: 15px;">7</div>
-        <div style="background-color: #d9d9d9; padding: 15px;">8</div>
-        <div style="background-color: #d9d9d9; padding: 15px;">9</div>
-    </div>
-</div>
-
-<div class="u-block u-block--grey">
-    <h3>Consecutive grids inside of u-block--grey</h3>
-    <div class="u-grid u-grid--2">
-        <div style="background-color: #d9d9d9; padding: 15px;">1</div>
-        <div style="background-color: #d9d9d9; padding: 15px;">2</div>
-        <div style="background-color: #d9d9d9; padding: 15px;">3</div>
-        <div style="background-color: #d9d9d9; padding: 15px;">4</div>
-        <div style="background-color: #d9d9d9; padding: 15px;">5</div>
-    </div>
-    <div class="u-grid u-grid--3">
-        <div style="background-color: #d9d9d9; padding: 15px;">1</div>
-        <div style="background-color: #d9d9d9; padding: 15px;">2</div>
-        <div style="background-color: #d9d9d9; padding: 15px;">3</div>
-        <div style="background-color: #d9d9d9; padding: 15px;">4</div>
-        <div style="background-color: #d9d9d9; padding: 15px;">5</div>
-        <div style="background-color: #d9d9d9; padding: 15px;">6</div>
-        <div style="background-color: #d9d9d9; padding: 15px;">7</div>
-    </div>
-    <div class="u-grid u-grid--4">
-        <div style="background-color: #d9d9d9; padding: 15px;">1</div>
-        <div style="background-color: #d9d9d9; padding: 15px;">2</div>
-        <div style="background-color: #d9d9d9; padding: 15px;">3</div>
-        <div style="background-color: #d9d9d9; padding: 15px;">4</div>
-        <div style="background-color: #d9d9d9; padding: 15px;">5</div>
-        <div style="background-color: #d9d9d9; padding: 15px;">6</div>
-        <div style="background-color: #d9d9d9; padding: 15px;">7</div>
-        <div style="background-color: #d9d9d9; padding: 15px;">8</div>
-        <div style="background-color: #d9d9d9; padding: 15px;">9</div>
-    </div>
+    <p>fsafasgfas</p>
+    <p>fsafasgfas</p>
 </div>
--- index.php
+++ index.php
@@ -3,10 +3,10 @@ require 'header.php';
 
 echo '<main>';
 require '_examples/grid.php';
-require '_examples/blocks.php';
+// require '_examples/blocks.php';
 require '_examples/content.php';
 // require '_examples/images.php';
-require '_examples/wp-images.php';
+// require '_examples/wp-images.php';
 echo '</main>';
 
 require 'footer.php';
